3acdae8819baeaf38ceabc060ed738320b412eab
leleslebson <lebson@hotmail.com>
2026-04-04 20:21:41 -0300

    Update appcv.py

--- appcv.py
+++ appcv.py
@@ -24,7 +24,7 @@ from streamlit_option_menu import option_menu
 # CONFIGURAÇÃO DA PÁGINA
 # ══════════════════════════════════════════
 st.set_page_config(
-    page_title="CV Intelgente ",
+    page_title="CV Inteligente ",
     page_icon="🪪",
     layout="wide",
     initial_sidebar_state="expanded"
